30c4d04d61dca3fd3b4bd8a1371b124a349d356f
Juan Castillo <Juanpablodlcc@gmail.com>
2022-12-27 19:00:02 -0600

    release

--- functions/index.js
+++ functions/index.js
@@ -11,7 +11,7 @@ const { Client } = require('pg')
 const api = express()
 const client = new Client({
     user: 'postgres',
-    host: '34.125.107.75',
+    host: '34.125.69.44',
     database: 'inhouse_bot',
     password: 'j8R0!&6nM90d1',
     port: 5432,
@@ -121,6 +121,83 @@ api.post("/match-history", async (req, res) => {
 
 
         res.send({ payload: hash_map });
+    } catch (err) {
+        console.log(err)
+        res.sendStatus(400)
+    }
+    res.end()
+})
+api.post("/profile/:uid", async (req, res) => {
+    try {
+        const player_id = req.params.uid
+        if (!player_id) {
+            res.send({ payload: {} });
+            return
+        }
+        const { rows: games } = await client.query(`
+        SELECT 
+            game.id,
+            game.blue_expected_winrate,
+            game_participant.player_id,
+            game_participant.side,
+            game_participant.name,
+            game_participant.role,
+            game.winner
+        FROM game
+        INNER JOIN game_participant
+        ON game.id=game_participant.game_id
+        WHERE winner is not NULL
+        `)
+
+        const { rows: [rating] } = await client.query(`
+        SELECT 
+            player.name,
+            player.id,
+            player_rating.trueskill_mu, 
+            player_rating.trueskill_sigma, 
+            player_rating.role
+            
+        FROM player
+        INNER JOIN player_rating
+        ON player.id=player_rating.player_id
+        WHERE id=${player_id}
+        `)
+
+
+        const { rows: history } = await client.query(`
+        SELECT 
+          *
+        FROM game_participant
+        INNER JOIN game
+        ON game.id=game_participant.game_id
+        WHERE player_id=${player_id}
+        `)
+
+
+        const players_matchs = history
+        const wins = players_matchs.filter(match => match.winner === match.side)
+        const looses = players_matchs.length - wins.length
+
+        const player_games = games.filter(game => game.player_id === player_id).map(game => game.id)
+        const hash_map = games
+            .filter(game => player_games.includes(game.id))
+            .reduce((hash, row) => ({
+                ...hash, [row.id]: [
+                    ...(hash[row.id] || []),
+                    row
+                ]
+            }), {})
+
+            const profile = {
+                ...rating,
+                looses: looses,
+                wins: wins.length,
+                nick: rating.name.toUpperCase(),
+                mmr: Number((20 * (rating.trueskill_mu - 3 * rating.trueskill_sigma + 25)).toFixed())
+
+            }
+
+        res.send({ profile, payload: hash_map });
     } catch (err) {
         console.log(err)
         res.sendStatus(400)
